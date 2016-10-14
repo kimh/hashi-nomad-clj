929bf2eb93b1ec80ec3fdf4882708844ef80518d
Kim, Hirokuni <yangkookkim@gmail.com>
2016-10-14 17:18:42 +0900

    wip

---
+++ test/hashi_nomad_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns hashi-nomad-clj.core-test
+  (:require [clojure.test :refer :all]
+            [hashi-nomad-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
